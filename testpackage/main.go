9174b95ccaf43dc872e7945cfe14ea1c3355a19b
uqingu <uqingu@foxmail.com>
2022-07-02 08:11:20 +0800

    test package

---
+++ testpackage/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	u "testpackage/utils"
+)
+
+func main() {
+	fmt.Println(u.Version)
+	println(u.FToC(100))
+}
---
+++ testpackage/utils/version.go
@@ -0,0 +1,4 @@
+package utils
+
+// Version is the current package version.
+const Version = "v0.0.1"
