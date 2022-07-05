fcb5ceb99c16bec0ba81a8984233de3540d41612
lyonmu <lyonmu@foxmail.com>
2022-07-05 14:49:26 +0800

    test gookit/slog

--- testone/main.go
+++ testone/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"github.com/6tail/lunar-go/calendar"
+	"github.com/gookit/slog"
 )
 
 func main() {
-	lunar := calendar.NewLunarFromYmd(1998, 8, 11)
-	fmt.Println(lunar.ToFullString())
-	fmt.Println(lunar.GetSolar().ToFullString())
+	//lunar := calendar.NewLunarFromYmd(1998, 8, 11)
+	//fmt.Println(lunar.ToFullString())
+	//fmt.Println(lunar.GetSolar().ToFullString())
+	// use JSON formatter
+	slog.Configure(func(logger *slog.SugaredLogger) {
+		f := logger.Formatter.(*slog.TextFormatter)
+		f.EnableColor = true
+	})
+
+	slog.Trace("this is a simple log message")
+	slog.Debug("this is a simple log message")
+	slog.Info("this is a simple log message")
+	slog.Notice("this is a simple log message")
+	slog.Warn("this is a simple log message")
+	slog.Error("this is a simple log message")
+	slog.Fatal("this is a simple log message")
 }
