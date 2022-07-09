062e1086697c522203f4cfe1591ac9c28976f18d
lyonmu <lyonmu@foxmail.com>
2022-07-09 10:08:00 +0800

    json格式化输出

--- testgopostgresql/main.go
+++ testgopostgresql/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"testgopostgresql/setting"
 )
 
 func main() {
 	fmt.Println("hello world!!!")
 	setting.Init()
-	fmt.Println(setting.SiteInfo)
+	// 普通输出结构体对象信息
+	// fmt.Println(setting.SiteInfo)
+
+	// JSON 格式输出结构体对象信息
+	data, err := json.MarshalIndent(setting.SiteInfo, "", "    ")
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
 }
